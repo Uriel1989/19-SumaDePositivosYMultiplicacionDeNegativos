bb0ae86d1e467064ad3e9f80891fce48465f75e8
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:33:13 -0300

    Add files via upload

---
+++ 19-SumaDePositivosYMultiplicacionDeNegativos/19-SumaDePositivosYMultiplicacionDeNegativos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _19_SumaDePositivosYMultiplicacionDeNegativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, a, b, c;
+            b = 1;
+            c = 0;
+
+            Console.WriteLine("Ingresar 10 numeros ");
+
+
+            for (i = 1; 1 <= 10; i++)
+            {
+
+                i = Convert.ToInt32(Console.ReadLine());
+
+                if (i > 0)
+                {
+                    c = c + i;
+                    Console.WriteLine("La suma de los numeros positivos da " + c);
+                }
+                else
+                if (i < 0)
+                {
+                    b = b * i;
+                    Console.WriteLine("El producto de los numeros negativos da " + b);
+                }
+            }
+
+            Console.WriteLine("La suma de los numeros positivos da " + c);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
